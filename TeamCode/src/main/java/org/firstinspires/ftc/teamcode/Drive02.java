d294d6a7a6f380b5dbe2a92f403e43df2c3d91fc
shadowdoge <william.y.xuan@gmail.com>
2016-12-26 14:44:14 -0800

    William

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive02.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive02.java
@@ -5,33 +5,49 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-/* (V 2.0 of drivetrain program; incl alexanders suggestions (thx alexander); so partially pressing down a button will
- * also make the servos go partially.
+/* (V 2.1 of drivetrain program)
+ * Features:
+ * Future proofed code for other, advanced functions (e.g. capping, pushing buttons, etc)
  * Basic program for Victanus's new drive train.
  * Basics:
  *  Forward - gamepad 2a, 1a = 1 (pressed) Motors left, right = 1
  * Turn right  - gamepad 2a, 1b = 1 (pressed) Motor right = -1, motor left = 1
  * Turn left  -  gamepad 2b, 1a = 1 (pressed) Motor right = 1, motor left = -1
  * Backward - gampad 2b, 1b = 1 (pressed) Motor right = -1, motor left = -1
-*  else - Motors left, right = 0
+ *  else - Motors left, right = 0
+ *  Special Functions:
+ *  Gamepad.x - button push - pushing x will push the button; default is retracted
+ *  left stick y - capping - pushing upwards and downwards on the y-axis f controller will change
+ *  the height of the capping mechanism
+ *  right stick y - intake mechanism - pushing upwards
+ *  Gamepad.b - shoot ball -
+ *  <code that is commented out and indented will be added later>
  */
-@TeleOp(name = "Drive02", group = "Tutorials")
-class Drive02 extends LinearOpMode
+@TeleOp(name = "DriveTraincontrol", group = "Tutorials")
+class Drive03 extends LinearOpMode
 {
     // Declare drive motors
     private DcMotor motorLeft;
     private DcMotor motorRight;
+    //private DcMotor capperMotor;
+    //Declare servo motor // TODO: 12/25/16
+    //private Servo buttonServo;
+
     @Override
     public void runOpMode() throws InterruptedException
     {
         // Initialize drive motors
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorRight = hardwareMap.dcMotor.get("motorRight");
-
+        //capperMotor = hardwareMap.dcMotor.get("capperMotor")
+        // TODO: 12/25/16 Declare
+        //buttonServo = hardwareMap.servo.get("buttonServo");
         // If drive motors are given full power, robot would spin because of the motors being in
         // opposite directions. So reverse one
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
-
+        //Declare positions of buttonServo // TODO: 12/25/16
+        //private static final double ARM_RETRACTED_POSITION = 0.2;
+        //private static final double ARM_EXTENDED_POSITION = 0.8;
 
         // Wait until start button is pressed
         waitForStart();
@@ -39,6 +55,11 @@ class Drive02 extends LinearOpMode
         // Repeatedly run code in here until stop button is pressed
         while(opModeIsActive())
         {
+            //Push button // TODO: 12/25/16
+                /*if (gamepad.x)
+                {
+                    buttonServo.setPosition(ARM_EXTENDED_POSITION);
+                */
             // Move drivetrain if requested
             if (gamepad1.a && gamepad2.a)
             {
@@ -63,9 +84,12 @@ class Drive02 extends LinearOpMode
                 motorLeft.setPower(-1);
                 motorRight.setPower(1);
             }
-            else
+            else {
                 motorLeft.setPower(0);
-            motorRight.setPower(0);
+                motorRight.setPower(0);
+                //arm resets to default // TODO: 12/25/16
+                //pushButton.setPosition(ARM_RETRACTED_POSITION);
+            }
             // Give hardware a chance to catch up
             idle();
         }
