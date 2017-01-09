3fb19b2a93a9bbc692cca66e6e9ef5b0315aaf82
Alexander <arghalexander3000@gmail.com>
2017-01-08 17:36:59 -0800

    All the memes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SoundTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SoundTest.java
@@ -7,15 +7,25 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import java.math.*;
+
+import static java.lang.Math.random;
+
 @TeleOp(name = "SoundTest", group = "Iterative Opmode")
 class SoundTest extends OpMode {
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
 
     public SoundPool mySound;
-    public int beepID;
+    public int xpstartupsound;
+    public int shadow;
+    public int yee;
+    public int screammeme;
+    public int one;
+    public int zylon;
 
     int timesPressed = 0;
+    boolean backPrev = false;
 
     @Override
     public void init() {
@@ -23,15 +33,39 @@ class SoundTest extends OpMode {
 
         //noinspection deprecation
         mySound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-        beepID = mySound.load(hardwareMap.appContext, R.raw.startupsoundxp, 1);
+        xpstartupsound = mySound.load(hardwareMap.appContext, R.raw.startupsoundxp, 1);
+        shadow = mySound.load(hardwareMap.appContext, R.raw.shadow, 1);
+        yee = mySound.load(hardwareMap.appContext, R.raw.yee, 1);
+        screammeme = mySound.load(hardwareMap.appContext, R.raw.screammeme, 1);
+        one = mySound.load(hardwareMap.appContext, R.raw.one, 1);
+        zylon = mySound.load(hardwareMap.appContext, R.raw.zylon, 1);
     }
 
     @Override
     public void loop() {
-        if (gamepad1.dpad_left) {
-            mySound.play(beepID,1,1,1,0,1);
-            timesPressed = timesPressed + 1;
+        telemetry.addData("Status", "Initialized");
+        if (gamepad1.back) {
+            mySound.play(xpstartupsound, 1, 1, 1, 0, 1);
         }
-        telemetry.addData("Times Pressed", timesPressed);
+        if (gamepad1.a) {
+            mySound.play(shadow, 1, 1, 1, 0, 1);
+        }
+        if (gamepad1.b) {
+            mySound.play(yee, 1, 1, 1, 0, 1);
+        }
+        if (gamepad1.y) {
+            mySound.play(screammeme, 1, 1, 1, 0, 1);
+        }
+        if (gamepad1.x) {
+            mySound.play(one, 1, 1, 1, 0, 1);
+        }
+        if (gamepad1.dpad_up) {
+            mySound.play(zylon, 1, 1, 1, 0, 1);
+        }
+        if (gamepad1.dpad_down) {
+            for (int n = 0; n < 7; n++) {
+                mySound.stop(n);
+            }
+        }
     }
 }
