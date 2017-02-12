b53dcd986de0f6593d0ec6d8d079800ddc5eb668
Alexander Youngblood <arghalexander3000@gmail.com>
2017-02-12 15:58:51 -0800

    Cool shooter things

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous04Blue.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Autonomous04Blue extends LinearOpMode {
+
+    GlobalFunctions g;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        g.driveFor(1);
+
+
+    }
+}
