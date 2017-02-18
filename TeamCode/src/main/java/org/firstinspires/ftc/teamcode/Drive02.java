5bd319d96b775dacf9e76a317ac9b2f85109684a
Alexander Youngblood <arghalexander3000@gmail.com>
2017-02-18 15:17:29 -0800

    Standardization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive02.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive02.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.hardware.DcMotorController;
@@ -15,48 +16,101 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import java.math.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @TeleOp(name = "Drive02", group = "Iterative Opmode")
 class Drive02 extends OpMode {
 
-    GlobalFunctions g;
+    public ElapsedTime runtime = new ElapsedTime();
+    public DcMotorController motor_controller_shooter;
+    public DcMotor shooter_motor_1;
+    public DcMotor shooter_motor_2;
+    //public DcMotor intake_motor_1;
+    public DcMotorController motor_controller_belt;
+    public DcMotor belt_motor;
+    public DcMotor intake_motor;
+    public Servo intake_servo;
+    public ServoController intake_servo_controller;
+    public DcMotorController motor_controller_drive;
+    public DcMotor motor_drive_right;
+    public DcMotor motor_drive_left;
+
+    public double power_motor_drive_right;
+    public double power_motor_drive_left;
+
+    public boolean speed;
+    public boolean startPrev;
+    public boolean startButton;
+    public boolean buttonRBPrev;
+    public boolean buttonLBPrev;
+
+    public boolean button_b;
+    public boolean button_y;
+    public boolean button_LB;
+    public boolean button_RB;
+
+    double instant;
 
     @Override
     public void init() {
         telemetry.clear();
         telemetry.addData("Status", "Initialized");
         // Initialize drive motors
-        g.motor_drive_left = hardwareMap.dcMotor.get("Motor_Drive_Left");
-        g.motor_drive_right = hardwareMap.dcMotor.get("Motor_Drive_Right");
-        g.shooter_motor_1 = hardwareMap.dcMotor.get("Shooter_Motor_1");
-        g.shooter_motor_2 = hardwareMap.dcMotor.get("Shooter_Motor_2");
-        //g.motor_hat = hardwareMap.dcMotor.get("Hat_Motor");
+        motor_controller_shooter = hardwareMap.dcMotorController.get("Motor_Controller_Shooter");
+        motor_controller_belt = hardwareMap.dcMotorController.get("Motor_Controller_Belt");
+        motor_controller_drive = hardwareMap.dcMotorController.get("Motor_Controller_Drive");
+        intake_servo_controller = hardwareMap.servoController.get("Servo_Controller_Intake");
 
-        g.motor_controller_drive = hardwareMap.dcMotorController.get("Motor_Controller_Drive");
-        g.motor_controller_shooter = hardwareMap.dcMotorController.get("Motor_Controller_Shooter");
-        g.motor_controller_other = hardwareMap.dcMotorController.get("Motor_Controller_Other");
-        //capperMotor = hardwareMap.dcMotor.get("capperMotor")
-        //
-        //buttonServo = hardwareMap.servo.get("buttonServo");
-        // If drive motors are given full power, robot would spin because of the motors being in
-        // opposite directions. So reverse one
-        g.motor_drive_left.setDirection(DcMotorSimple.Direction.REVERSE);
-        g.motor_drive_right.setDirection(DcMotorSimple.Direction.FORWARD);
-        g.shooter_motor_1.setDirection(DcMotorSimple.Direction.FORWARD);
-        g.shooter_motor_2.setDirection(DcMotorSimple.Direction.REVERSE);
-        //Declare positions of buttonServo //
+        shooter_motor_1 = hardwareMap.dcMotor.get("Motor_Shooter_1");
+        shooter_motor_2 = hardwareMap.dcMotor.get("Motor_Shooter_2");
 
-        gamepad1.setJoystickDeadzone((float)0.2);
+        belt_motor = hardwareMap.dcMotor.get("Motor_Belt");
+        intake_motor = hardwareMap.dcMotor.get("Motor_Intake");
+
+        intake_servo = hardwareMap.servo.get("Servo_Intake");
+
+        motor_drive_right = hardwareMap.dcMotor.get("Motor_Drive_Right");
+        motor_drive_left = hardwareMap.dcMotor.get("Motor_Drive_Left");
+
+        shooter_motor_1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        shooter_motor_2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        motor_drive_right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        motor_drive_left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        belt_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake_motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+//        intake_motor_1 = hardwareMap.dcMotor.get("Motor_Intake_1");
+        gamepad1.setJoystickDeadzone((float) 0.1);
+        shooter_motor_1.setDirection(DcMotorSimple.Direction.REVERSE);
+        shooter_motor_2.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        motor_drive_right.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor_drive_left.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motor_drive_right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor_drive_left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        belt_motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        intake_motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        shooter_motor_1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooter_motor_2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        power_motor_drive_right = 0;
+        power_motor_drive_left = 0;
+        intake_servo.setPosition(0);
 
         /*
         //noinspection deprecation
-        g.mySound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
-        g.beepID = g.mySound.load(hardwareMap.appContext, R.raw.startupsoundxp, 1);
-        g.mySound.play(g.beepID,1,1,1,0,1);
+        mySound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        beepID = mySound.load(hardwareMap.appContext, R.raw.startupsoundxp, 1);
+        mySound.play(beepID,1,1,1,0,1);
         */
 
-        g.speed = false;
-        g.startPrev = false;
+        speed = false;
+        startPrev = false;
+
+        buttonRBPrev = false;
+        buttonLBPrev = false;
     }
 
     @Override
@@ -64,103 +118,112 @@ class Drive02 extends OpMode {
         telemetry.clear();
         telemetry.addData("Status", "In Loop");
 
-        g.startButton = gamepad1.start;
-        g.left_trigger = -gamepad1.left_stick_y;
-        g.right_trigger = -gamepad1.right_stick_y;
-        g.button_b = gamepad1.b;
-        g.button_LB = gamepad1.left_bumper;
-        g.button_RB = gamepad1.right_bumper;
+        startButton = gamepad1.start;
+        power_motor_drive_left = -gamepad1.left_stick_y;
+        power_motor_drive_right = -gamepad1.right_stick_y;
+        button_b = gamepad2.b;
+        button_y = gamepad2.y;
+        button_LB = gamepad2.left_bumper;
+        button_RB = gamepad2.right_bumper;
 
-        if (g.startButton && !g.startPrev) {
-            g.speed = true;
+        drive_control();
+        shooter_control();
+        intake_control();
+
+        telemetry.addData("Speed:", speed);
+        telemetry.addData("Start Button:", startButton);
+        telemetry.addData("Start Button Previous", startPrev);
+        //telemetry.addData("Left DPad:", gamepad1.dpad_left);
+        telemetry.addData("Left Trigger", power_motor_drive_left);
+        telemetry.addData("Right Trigger", power_motor_drive_right);
+
+        startPrev = startButton;
+
+        motor_drive_left.setPower(power_motor_drive_left);
+        motor_drive_right.setPower(power_motor_drive_right);
+    }
+
+    public void drive_control() {
+        if (startButton && !startPrev) {
+            speed = true;
         } else {
-            g.speed = false;
+            speed = false;
         }
 
 
 
-        //g.mySound.play(g.beepID,1,1,1,0,1);
-
-
-
-        if (Math.pow(g.left_trigger, 3) > 0) {
-            g.left_trigger = Math.max(Math.pow(g.left_trigger, 3), 0.7);
-        } else if (Math.pow(g.left_trigger, 3) < 0) {
-            g.left_trigger = Math.min(Math.pow(g.left_trigger, 3), -0.7);
+        if (Math.pow(power_motor_drive_left, 3) > 0) {
+            power_motor_drive_left = Math.max(Math.pow(power_motor_drive_left, 3), 0.7);
+        } else if (Math.pow(power_motor_drive_left, 3) < 0) {
+            power_motor_drive_left = Math.min(Math.pow(power_motor_drive_left, 3), -0.7);
         } else {
-            g.left_trigger = 0;
+            power_motor_drive_left = 0;
         }
 
-        if (Math.pow(g.right_trigger, 3) > 0) {
-            g.right_trigger = Math.max(Math.pow(g.right_trigger, 3), 0.7);
-        } else if (Math.pow(g.right_trigger, 3) < 0) {
-            g.right_trigger = Math.min(Math.pow(g.right_trigger, 3), -0.7);
+        if (Math.pow(power_motor_drive_right, 3) > 0) {
+            power_motor_drive_right = Math.max(Math.pow(power_motor_drive_right, 3), 0.7);
+        } else if (Math.pow(power_motor_drive_right, 3) < 0) {
+            power_motor_drive_right = Math.min(Math.pow(power_motor_drive_right, 3), -0.7);
         } else {
-            g.right_trigger = 0;
+            power_motor_drive_right = 0;
         }
 
-
-        /*
-        //Push button //
-        if (gamepad1.a)
-        {
-  //          buttonServo.setPosition(ARM_EXTENDED_POSITION);
-
-        }
-        else {
-            //arm resets to default //
-    //        buttonServo.setPosition(ARM_RETRACTED_POSITION);
-        }
-        */
-
-
-        /*
-        if (gamepad1.x) {
-            g.left_trigger = 0;
-            g.right_trigger = 0;
-            g.driveFor(0.5, 1.5);
-        }
-        */
-
-        if (g.speed) {
-            g.left_trigger = g.left_trigger / 2;
-            g.right_trigger = g.right_trigger / 2;
+        if (speed) {
+            power_motor_drive_left = power_motor_drive_left / 2;
+            power_motor_drive_right = power_motor_drive_right / 2;
             telemetry.addData("Speed", "Slow");
         } else {
             telemetry.addData("Speed", "Fast");
         }
+    }
 
-        if (g.button_b) {
-            g.shooter_motor_1.setPower(0.5);
-            g.shooter_motor_2.setPower(0.5);
+    public void intake_control() {
+        if (button_RB && !buttonRBPrev) {
+            intake_motor.setPower(1);
+            belt_motor.setPower(1);
+            buttonRBPrev = button_RB;
         } else {
-            g.shooter_motor_1.setPower(0);
-            g.shooter_motor_2.setPower(0);
+            intake_motor.setPower(0);
+            belt_motor.setPower(0);
+        }
+    }
+
+    public void shooter_control() {
+        if (button_y) {
+            intake_servo.setPosition(0);
+            shooter_motor_1.setPower(1);
+            shooter_motor_2.setPower(1);
+            waitFor(2000);
+            belt_motor.setPower(1);
+            waitFor(500);
+            intake_servo.setPosition(0.05);
+            waitFor(2);
+            intake_servo.setPosition(0);
+            shooter_motor_1.setPower(0);
+            shooter_motor_2.setPower(0);
+            belt_motor.setPower(0);
+        } else {
+            intake_servo.setPosition(0);
         }
 
-        if (g.button_LB) {
-            g.intake_motor.setPower(1);
+        if (button_LB && !buttonLBPrev) {
+            shooter_motor_1.setPower(0.3);
+            shooter_motor_2.setPower(0.3);
+            buttonLBPrev = button_LB;
         } else {
-            g.intake_motor.setPower(0);
+            shooter_motor_1.setPower(0);
+            shooter_motor_2.setPower(0);
         }
+    }
 
-        if (g.button_RB) {
-            g.belt_motor.setPower(1);
-        } else {
-            g.belt_motor.setPower(0);
-        }
 
-        telemetry.addData("Speed:", g.speed);
-        telemetry.addData("Start Button:", g.startButton);
-        telemetry.addData("Start Button Previous", g.startPrev);
-        //telemetry.addData("Left DPad:", gamepad1.dpad_left);
-        telemetry.addData("Left Trigger", g.left_trigger);
-        telemetry.addData("Right Trigger", g.right_trigger);
+    public void waitFor(long seconds) {
+            seconds = seconds * 1000;
 
-        g.startPrev = g.startButton;
-
-        g.motor_drive_left.setPower(g.left_trigger);
-        g.motor_drive_right.setPower(g.right_trigger);
+            instant = runtime.milliseconds();
+            while (instant > runtime.milliseconds() - seconds) {
+                telemetry.addData("Time Left", (seconds - (runtime.milliseconds() - instant)));
+            }
     }
 
 }
