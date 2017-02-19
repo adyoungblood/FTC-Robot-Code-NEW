c3ecd7d7770e0f5db1de2e6955b5968a80bb8206
Alexander Youngblood <arghalexander3000@gmail.com>
2017-02-19 09:53:47 -0800

    a ton of moose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous04Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous04Blue.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
@@ -8,6 +9,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Disabled
 @Autonomous
 public class Autonomous04Blue extends LinearOpMode {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous04Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous04Red.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
@@ -9,6 +10,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Disabled
 @Autonomous
 public class Autonomous04Red extends LinearOpMode {
 
